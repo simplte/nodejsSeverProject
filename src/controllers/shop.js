c0e87370ae1502304239e99fca5609e1d4f13e7d
bqc <6575734@qq.com>
2020-08-04 23:09:15 +0800

    模块分层

--- src/controllers/shop.js
+++ src/controllers/shop.js
@@ -1,62 +1,66 @@
+// src/controllers/shop.js
 const { Router } = require('express');
-
 const shopService = require('../services/shop');
 
 class ShopController {
 	shopService;
-	async init() {
-		this.shopService = await shopService();
-		const router = Router();
-		router.get('/', this.getAll);
-		router.get('/:shopId', this.getOne);
-		router.put('/:shopId', this.put);
-		router.delete('/shopId', this.delete);
-		return router;
-	}
+  async init() {
+    this.shopService = await shopService();
 
-	getAll = async (req, res) => {
-		const { pageIndex, pageSize } = req.query;
-		const shopList = await this.shopService.find({ pageIndex, pageSize });
-		res.send({ success: true, data: shopList });
-	};
+    const router = Router();
+    router.get('/', this.getAll);
+    router.get('/:shopId', this.getOne);
+    router.put('/:shopId', this.put);
+    router.delete('/:shopId', this.delete);
+    return router;
+  }
 
-	getOne = async (req, res) => {
-		const { shopId } = req.params;
-		const shopList = await this.shopService.find({ id: shopId });
-		if (shopList.length) {
-			res.send({ success: true, data: shopList[0] });
-		} else {
-			res.status(404).send({ success: false, data: null });
-		}
-	};
+   async getAll(req, res) {
+	const { pageIndex, pageSize } = req.query;
+	console.log(this.shopService)
+    const shopList = await this.shopService.find({ pageIndex, pageSize });
 
-	put = async (req, res) => {
-		const { shopId } = req.params;
-		const { name } = req.query;
-		const shopInfo = await this.shopService.modify({
-			id: shopId,
-			value: { name }
-		});
-		if (shopInfo) {
-			res.send({ success: true, data: shopInfo });
-		} else {
-			res.status(404).send({
-				success: false,
-				data: null
-			});
-		}
-	};
+    res.send({ success: true, data: shopList });
+  };
 
-	delete = async (req, res) => {
-		const { shopId } = req.params;
-		const success = await this.shopService.delete({ id: shopId });
-		if (!success) {
-			res.status(404);
-		}
-		res.send({ success });
-	};
+  async getOne(req, res) {
+    const { shopId } = req.params;
+    const shopList = await this.shopService.find({ id: shopId });
+
+    if (shopList.length) {
+      res.send({ success: true, data: shopList[0] });
+    } else {
+      res.status(404).send({ success: false, data: null });
+    }
+  };
+
+  async put(req, res) {
+    const { shopId } = req.params;
+    const { name } = req.query;
+    const shopInfo = await this.shopService.modify({
+      id: shopId,
+      values: { name },
+    });
+
+    if (shopInfo) {
+      res.send({ success: true, data: shopInfo });
+    } else {
+      res.status(404).send({ success: false, data: null });
+    }
+  };
+
+  async delete(req, res) {
+    const { shopId } = req.params;
+    const success = await this.shopService.remove({ id: shopId });
+
+    if (!success) {
+      res.status(404);
+    }
+    res.send({ success });
+  };
 }
+
 module.exports = async () => {
-    const c = new ShopController();
-    return await c.init();
-}
+  const c = new ShopController();
+  return await c.init();
+};
