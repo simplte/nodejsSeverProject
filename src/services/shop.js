c3fb24a3f65b6cde0212507c1be0b1831166b7c2
bqc <6575734@qq.com>
2020-08-11 23:14:57 +0800

    数据库

--- src/services/shop.js
+++ src/services/shop.js
@@ -1,53 +1,62 @@
-const memoryStorage = {
-	'1001': { name: '良品铺子' },
-	'1002': { name: '来伊份' },
-	'1003': { name: '三只松鼠' },
-	'1004': { name: '百草味' },
-	'1005': { name: '零食铺子' }
-};
-async function delay(ms = 200) {
-	await new Promise(r => setTimeout(r, ms));
-}
-
+// const memoryStorage = {
+// 	'1001': { name: '良品铺子' },
+// 	'1002': { name: '来伊份' },
+// 	'1003': { name: '三只松鼠' },
+// 	'1004': { name: '百草味' },
+// 	'1005': { name: '零食铺子' }
+// };
+// async function delay(ms = 200) {
+// 	await new Promise(r => setTimeout(r, ms));
+// }
+const { Shop } = require('../models');
 class ShopService {
-	async init() {
-		await delay();
-	}
+	async init() {}
 
 	async find({ id, pageIndex = 0, pageSize = 10 }) {
-		await delay();
+		// await delay();
 		if (id) {
 			// 过滤掉假值
 			// [memoryStorage[id]].filter(function (x) { return Boolean(x); });
-			return [memoryStorage[id]].filter(Boolean);
+			// return [memoryStorage[id]].filter(Boolean);
+			return [await Shop.findByPk(id)];
 		}
-		return Object.keys(memoryStorage)
-			.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize)
-			.map(id => ({ id, ...memoryStorage[id] }));
+		// return Object.keys(memoryStorage)
+		// 	.slice(pageIndex * pageSize, (pageIndex + 1) * pageSize)
+		// 	.map(id => ({ id, ...memoryStorage[id] }));
+		return await Shop.findAll({
+		  offset: pageIndex * pageSize,
+		  limit: pageSize,
+		});
 	}
 
 	async modify({ id, values }) {
-		await delay();
-		const target = memoryStorage[id];
+		// await delay();
+		// const target = memoryStorage[id];
+		const target = await Shop.findByPk(id);
 		if (!target) {
 			return null;
 		}
-		return Object.assign(target, values);
+		// return Object.assign(target, values);
+	    Object.assign(target, values);
+	    return await target.save();
 	}
 
 	async remove({ id }) {
-		await delay();
-		const target = memoryStorage[id];
+		// await delay();
+		// const target = memoryStorage[id];
+		const target = await Shop.findByPk(id);
 		if (!target) {
 			return false;
 		}
-		return delete memoryStorage[id];
+		// return delete memoryStorage[id];
+		return target.destroy();
 	}
 
 	async create({ values }) {
-		await delay();
-		const id = String(1 + Object.keys(memoryStorage).reduce((m, id) => Math.max(m, id), -Infinity));
-		return { id, ...(memoryStorage[id] = values) };
+		// await delay();
+		// const id = String(1 + Object.keys(memoryStorage).reduce((m, id) => Math.max(m, id), -Infinity));
+		// return { id, ...(memoryStorage[id] = values) };
+		return await Shop.create(values)
 	}
 }
 // 单例模式
